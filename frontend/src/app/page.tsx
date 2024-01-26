f5c29f85b189bf036e9ac3d8b73b891a127aa5ea
bisrat-walle <bisratwalle3@gmail.com>
2024-01-26 14:28:57 +0300

    add basic layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from "../../components/Header";
 
 export default function Home() {
   return (
-    <main>
-
-    </main>
-  )
+    <div className="">
+      <Header />
+      <div className="bg-secondary">Hello World</div>
+    </div>
+  );
 }
