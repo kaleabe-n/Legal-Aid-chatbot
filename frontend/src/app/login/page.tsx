9e03234f0729a5ae72b4d8a3326d2bfa89db9770
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2024-01-26 20:48:49 +0300

    made login ui responsive

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,37 +1,92 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import React from "react";
 import { FiPhone } from "react-icons/fi";
 import { CiLock } from "react-icons/ci";
-import Image from "next/image";
 
-const LoginPage: React.FC = () => {
+const Page: React.FC = () => {
   return (
-    <div className="bg-secondary min-h-screen flex items-center justify-center">
-      <div className="w-9/12 flex shadow-md">
-        <div className="w-7/12 bg-primary flex flex-col justify-center items-center space-y-4 rounded-l-xl">
-          {/* Logo section */}
-          <Image
-            src="legal-aid-logo.svg"
-            alt=""
-            width={1}
-            height={1}
-            className="w-[100%]"
-          />
+    <>
+      <div className="hidden md:flex bg-secondary min-h-screen items-center justify-center">
+        <div className="w-9/12 flex shadow-md">
+          <div className="w-7/12 bg-primary flex flex-col justify-center items-center space-y-4 rounded-l-xl">
+            {/* Logo section */}
+            <img src="./images/login/Mask group.svg" alt="" className="" />
 
-          {/* Title Section */}
-          <h1 className="font-bold text-lg text-white">Legal Aid</h1>
+            {/* Title Section */}
+            <h1 className="font-bold text-lg text-white">Legal Aid</h1>
 
-          {/* Description section */}
-          <p className="text-white mt-4">
-            Ask about legal system with just clicks
-          </p>
+            {/* Description section */}
+            <p className="text-white mt-4">
+              Ask about legal system with just clicks
+            </p>
+          </div>
+          <div className="w-5/12 bg-white rounded-r-xl flex flex-col py-20 px-8">
+            {/* Welcome section */}
+            <h1 className="w-full text-center text-xl font-semibold">
+              Welcome
+            </h1>
+
+            {/* form section */}
+            <div className="flex flex-col space-y-4 px-8 mt-8">
+              <div className="border border-gray-300 rounded-md py-2 px-4 has-[:focus]:outline-none has-[:focus]:ring-2 has-[:focus]:ring-primary flex items-center space-x-3">
+                <FiPhone color="#505050" />
+                <input
+                  type="text"
+                  placeholder="Phone Number"
+                  className="flex-auto h-full focus:outline-none"
+                />
+              </div>
+              <div className="border border-gray-300 rounded-md py-2 px-4 has-[:focus]:outline-none has-[:focus]:ring-2 has-[:focus]:ring-primary flex items-center space-x-3">
+                <CiLock color="#000000" size={18} />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="flex-auto h-full focus:outline-none"
+                />
+              </div>
+            </div>
+
+            {/* Reset Password link */}
+            <div className="text-center mt-4">
+              <Link href="#" className="text-primary text-sm hover:underline">
+                Reset Password?
+              </Link>
+            </div>
+            <button className="mt-8 mx-8 bg-primary text-white rounded-md py-3 px-4 outline outline-2 outline-primary hover:bg-white hover:text-primary hover:shadow-inner transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-primary">
+              Login
+            </button>
+            <div className="flex items-center mt-4 px-5">
+              <hr className="w-full border-gray-800" />
+              <p className="mx-4">OR</p>
+              <hr className="w-full border-gray-800" />
+            </div>
+            <button className="mt-4 mx-8 rounded-md py-1 px-4 outline outline-1 outline-slate-500 text-sm bg-white hover:shadow-inner focus:outline-none focus:ring-2 focus:ring-primary flex space-x-2 items-center justify-center">
+              <img
+                src="./images/login/googleIcon.svg"
+                alt="google"
+                className="w-10 h-10 object-cover"
+              />
+              <p>Sign in With Google</p>
+            </button>
+            <div className="mt-4 px-8 flex justify-between">
+              <p className="text-sm text-slate-500">
+                Don&apos;t Have an account?
+              </p>
+              <Link href="#" className="text-primary text-sm hover:underline">
+                Sign up
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="w-5/12 bg-white rounded-r-xl flex flex-col py-20 px-8">
+      </div>
+      
+      <div className="md:hidden bg-white rounded-r-xl flex flex-col py-20 px-8">
           {/* Welcome section */}
-          <h1 className="w-full text-center text-xl font-semibold">Welcome</h1>
+          <h1 className="w-full text-center text-xl font-semibold">Legal Aid Login</h1>
 
           {/* form section */}
-          <div className="flex flex-col space-y-4 px-8 mt-8">
+          <div className="flex flex-col space-y-4 mt-8">
             <div className="border border-gray-300 rounded-md py-2 px-4 has-[:focus]:outline-none has-[:focus]:ring-2 has-[:focus]:ring-primary flex items-center space-x-3">
               <FiPhone color="#505050" />
               <input
@@ -41,7 +96,7 @@ const LoginPage: React.FC = () => {
               />
             </div>
             <div className="border border-gray-300 rounded-md py-2 px-4 has-[:focus]:outline-none has-[:focus]:ring-2 has-[:focus]:ring-primary flex items-center space-x-3">
-              <CiLock color="#000000" size={18} />
+            <CiLock color="#000000" size={18} />
               <input
                 type="password"
                 placeholder="Password"
@@ -51,20 +106,20 @@ const LoginPage: React.FC = () => {
           </div>
 
           {/* Reset Password link */}
-          <div className="text-center mt-4">
+          <div className="text-right mt-4">
             <Link href="#" className="text-primary text-sm hover:underline">
               Reset Password?
             </Link>
           </div>
-          <button className="mt-8 mx-8 bg-primary text-white rounded-md py-3 px-4 outline outline-2 outline-primary hover:bg-white hover:text-primary hover:shadow-inner transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-primary">
+          <button className="mt-8 bg-primary text-white rounded-md py-3 px-4 outline outline-2 outline-primary hover:bg-white hover:text-primary hover:shadow-inner transition ease-in duration-200 focus:outline-none focus:ring-2 focus:ring-primary">
             Login
           </button>
-          <div className="flex items-center mt-4 px-5">
+          <div className="flex items-center mt-4">
             <hr className="w-full border-gray-800" />
             <p className="mx-4">OR</p>
             <hr className="w-full border-gray-800" />
           </div>
-          <button className="mt-4 mx-8 rounded-md py-1 px-4 outline outline-1 outline-slate-500 text-sm bg-white hover:shadow-inner focus:outline-none focus:ring-2 focus:ring-primary flex space-x-2 items-center justify-center">
+          <button className="mt-4 rounded-md py-1 px-4 outline outline-1 outline-slate-500 text-sm bg-white hover:shadow-inner focus:outline-none focus:ring-2 focus:ring-primary flex space-x-2 items-center justify-center">
             <img
               src="./images/login/googleIcon.svg"
               alt="google"
@@ -76,17 +131,13 @@ const LoginPage: React.FC = () => {
             <p className="text-sm text-slate-500">
               Don&apos;t Have an account?
             </p>
-            <Link
-              href="signup"
-              className="text-primary text-sm hover:underline"
-            >
+            <Link href="#" className="text-primary text-sm hover:underline">
               Sign up
             </Link>
           </div>
         </div>
-      </div>
-    </div>
+    </>
   );
 };
 
-export default LoginPage;
+export default Page;
